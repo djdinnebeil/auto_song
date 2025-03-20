34912476f0adefc724094cc3fee53507029ca169
djdinnebeil <djdinnebeil@gmail.com>
2025-03-20 13:32:57 -0400

    Added timezone function

--- clock.py
+++ clock.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
+
+def get_current_time_in_timezone(timezone: str = "UTC"):
+    return datetime.now(ZoneInfo(timezone))
 
 def get_current_time():
     return datetime.now()
 
 if __name__ == '__main__':
     print(get_current_time())
+    print(get_current_time_in_timezone("America/New_York"))

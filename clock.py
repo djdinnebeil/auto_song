95b2b8b43b823173532a6d329502293d9227b608
djdinnebeil <djdinnebeil@gmail.com>
2025-03-20 18:45:59 -0400

    Added function for formatted datestamp

--- clock.py
+++ clock.py
@@ -7,6 +7,11 @@ def get_current_time_in_timezone(timezone: str = "UTC"):
 def get_current_time():
     return datetime.now()
 
+def get_formatted_date():
+    return datetime.today().strftime("%#m-%#d-%y")
+
+
 if __name__ == '__main__':
     print(get_current_time())
     print(get_current_time_in_timezone("America/New_York"))
+    print(get_formatted_date())

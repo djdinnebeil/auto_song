aaaf5bbd4da7967b037cc6441b57772c544caca4
djdinnebeil <djdinnebeil@gmail.com>
2025-03-21 15:08:40 -0400

    Added local time dict

--- clock.py
+++ clock.py
@@ -10,8 +10,20 @@ def get_current_time():
 def get_formatted_date():
     return datetime.today().strftime("%#m-%#d-%y")
 
+def get_local_time_dict():
+    now = datetime.now()
+    return {
+        'year': now.year,
+        'month': now.month,
+        'day': now.day,
+        'hour': now.hour,
+        'minute': now.minute,
+        'second': now.second,
+        'millisecond': int(now.microsecond / 1000)
+    }
 
 if __name__ == '__main__':
     print(get_current_time())
     print(get_current_time_in_timezone("America/New_York"))
     print(get_formatted_date())
+    print(get_local_time_dict()['hour'])

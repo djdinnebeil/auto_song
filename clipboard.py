278bb9350228ed5fdf4cfdde5f2d805e6ca6138c
djdinnebeil <djdinnebeil@gmail.com>
2025-03-26 11:03:39 -0400

    Added format_hour function

--- clipboard.py
+++ clipboard.py
@@ -3,3 +3,6 @@ import pyperclip
 def copy_to_clipboard(text: str):
     """Copy the given text to the system clipboard."""
     pyperclip.copy(text)
+
+if __name__ == '__main__':
+    copy_to_clipboard('hey ya')
--- clock.py
+++ clock.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+end_of_day = 6
+
 def get_current_time_in_timezone(timezone: str = "UTC"):
     return datetime.now(ZoneInfo(timezone))
 
@@ -27,9 +29,19 @@ def format_minutes(minutes: int):
         return f'0{minutes}'
     return f'{minutes}'
 
+def format_hour(hour: int):
+    if hour < end_of_day:
+        hour += 24
+    elif hour < 10:
+        return f'0{hour}'
+    elif 13 <= hour < 22:
+        hour -= 12
+    return f'{hour}'
+
 if __name__ == '__main__':
     print(get_current_time())
     print(get_current_time_in_timezone("America/New_York"))
     print(get_formatted_date())
     print(get_local_time_dict()['hour'])
     print(format_minutes(datetime.now().minute))
+    print(format_hour(datetime.now().hour))

315cc027ae27c8cd2a51a776c8b1ac08a8653c59
djdinnebeil <djdinnebeil@gmail.com>
2025-04-08 18:18:43 -0400

    Add paste_from_clipboard

--- clipboard.py
+++ clipboard.py
@@ -1,8 +1,20 @@
 import pyperclip
+import pyautogui
+from time import sleep
+
+pyautogui.FAILSAFE = False  # The failsafe only concerns mouse activity, so set to False
 
 def copy_to_clipboard(text: str):
     """Copy the given text to the system clipboard."""
     pyperclip.copy(text)
 
+def paste_from_clipboard():
+    pyautogui.keyDown('ctrlleft')
+    pyautogui.press('v')
+    pyautogui.keyUp('ctrlleft')
+
+
 if __name__ == '__main__':
     copy_to_clipboard('hey ya')
+    sleep(1)
+    paste_from_clipboard()

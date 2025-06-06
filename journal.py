57f1340a130ad20828b4bae952100aed3f705f0f
DJ <djdinnebeil@gmail.com>
2025-06-06 14:12:10 -0400

    Adding episode title function

--- journal.py
+++ journal.py
@@ -13,3 +13,10 @@ def get_am_pm_timestamp():
     if am_pm_timestamp[0] == '0':
         am_pm_timestamp = am_pm_timestamp[1:]
     return am_pm_timestamp
+
+def get_episode_title(title):
+    title = f'{title}\n'
+    title += f'Entry {get_datestamp()}\n'
+    title += f'{get_military_timestamp()}\n\n'
+    title += f'{get_am_pm_timestamp()}'
+    return title

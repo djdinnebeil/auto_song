39c17405cc74bc97a7b43e903c12fcba4869e37c
DJ <djdinnebeil@gmail.com>
2025-06-06 11:41:12 -0400

    Adding military timestamp function

--- journal.py
+++ journal.py
@@ -3,3 +3,7 @@ from datetime import datetime
 def get_datestamp():
     datestamp = datetime.now().strftime('%Y%m%d')
     return datestamp
+
+def get_military_timestamp():
+    military_timestamp = datetime.now().strftime('%H:%M:%S.%f')
+    return military_timestamp
